e8c9a20e278b227071831e21315e9abd02e21e3d
joyline-machado <joylineclaudia4@gmail.com>
2023-08-09 16:14:57 +0530

    connected with firebase

---
+++ src/app/recipes/recipe-resolver.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, inject } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve, ResolveFn, RouterStateSnapshot } from "@angular/router";
+import { Recipe } from "./recipe.model";
+import { DataStorageService } from "../shared/data-storage.service";
+import { Observable } from "rxjs";
+import { RecipeService } from "./recipe.service";
+
+// @Injectable()
+// export class RecipeResolverService implements Resolve<Recipe[]>{
+//   constructor(private dataStorageService: DataStorageService) { }
+
+//   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+//     return this.dataStorageService.fetchRecipes()
+//   }
+// }
+export const RecipeResolverService: ResolveFn<Recipe[]> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+): Observable<Recipe[]> | Promise<Recipe[]> | Recipe[] => {
+  const recipeService: RecipeService = inject(RecipeService);
+  const dataStorageService: DataStorageService = inject(DataStorageService);
+  const recipes: Recipe[] = recipeService.getRecipes();
+  if (recipes.length === 0) {
+    return dataStorageService.fetchRecipes();
+  } else {
+    return recipes;
+  }
+};
+
+
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -28,8 +28,15 @@ export class  RecipeService {
     )
   ];
 
+  // private recipes: Recipe[] = [];
+
   constructor(private slService: ShoppingListService) {
 
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
